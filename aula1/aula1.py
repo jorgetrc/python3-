de1f69c68a50bcdd5d0f79a8218d265154c0d1aa
jl <jorgetrc01@gmail.com>
2022-01-21 16:06:39 -0300

    formas de uso dos comentários

---
+++ aula1/aula1.py
@@ -0,0 +1,20 @@
+print('Linha1')
+print('Linha2')
+print('Linha3')  # Isso é um comentário.
+#  comentário de inicio de linha
+#  comentário 2
+print('Linha4')
+#  print('Linha5')
+#  print('Linha6')  codigo comentado
+"""
+Comentários em várias linhas... 
+linha 1 
+linha 2
+linha 3
+"""
+
+'''
+123465
+asdfg
+python
+'''
