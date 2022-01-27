fb82fa2daa11e841eb81fd5f6ac2bcf4da64da55
jl <jorgetrc01@gmail.com>
2022-01-27 18:13:39 -0300

    Formas de utilizar variáveis e utilizar o comando print formatado.

--- aula1/variaveis.py
+++ aula1/variaveis.py
@@ -13,4 +13,8 @@ print('Idade:', idade)
 print('Altura:', altura)
 print('É maior:', e_maior)
 
-print(nome, 'tem', idade, 'de idade e seu imc é', imc)
+print(nome, 'tem', idade, 'anos de idade e seu imc é', imc)
+
+print(f'{nome} tem {idade} anos de idade e seu imc é {imc:.2f}')
+
+print('{} tem {} anos de idade e seu imc é {:.2f}'.format(nome, idade, imc))
