c56a3b7c80c73c39798ef81f5929c98d77f4b070
jl <jorgetrc01@gmail.com>
2022-01-27 15:01:58 -0300

    Formas de utilizar o comando print.

---
+++ aula1/print.py
@@ -0,0 +1,13 @@
+
+print(123456)
+
+print('Jorge', 'Lucas')
+
+print('GNU', 'Linux', sep='-', end=' ##### ')
+print ('Linux', 'e', 'Python', sep='-', end='')
+
+"""
+Imprimindo números inteiros, strings, 
+utilizando o print com separadores e concatenando print com uso do end.
+
+"""
