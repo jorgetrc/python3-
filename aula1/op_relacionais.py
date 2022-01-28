0c21ef661da38d918dcd10151cae89167821c72b
jl <jorgetrc01@gmail.com>
2022-01-28 17:05:31 -0300

    Operadores relacionis.

---
+++ aula1/op_relacionais.py
@@ -0,0 +1,16 @@
+"""
+Operadores Relacionis
+== igualdade
+> maior que
+>= maior ou igual que
+< menor que
+<= menor que ou igual que
+!= diferente
+"""
+
+var_1 = 'JL'
+var_2 = 'MTT'
+
+expressao = (var_1 != var_2)
+
+print(expressao)
