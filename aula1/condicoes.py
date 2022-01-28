0ef5b936aaff07685260002992c9fdc3c92dd86e
jl <jorgetrc01@gmail.com>
2022-01-28 16:46:02 -0300

    Condições IF, ELIF e ELSE.

---
+++ aula1/condicoes.py
@@ -0,0 +1,12 @@
+"""
+Condições IF, ELIF e ELSE
+Para testar os resultados altere entre True e False nas condições.
+"""
+if False:
+    print('Verdadeiro.')
+elif False:
+    print('Agora é verdadeiro.')
+elif True:
+    print('Mais uma verdadeira.')
+else:
+    print('Não é verdadeiro.')
