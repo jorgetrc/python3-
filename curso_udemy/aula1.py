9f46fa536a59b2da4bdba90911b3dd36903e7b92
jorgetrc <jorgetrc01@gmail.com>
2023-04-11 10:39:56 -0300

    Python3 Udemy

---
+++ curso_udemy/aula1.py
@@ -0,0 +1,15 @@
+"""
+Docstrings, não é um comentário.
+Mas é utilizado, como comentário multi-linha.
+
+"""
+
+'''
+
+Docstrings, também podem ser iniciadas com aspas simples ''
+
+'''
+
+# um comentario
+print(12, 34) # comentario ao lado
+# comentario abaixo
