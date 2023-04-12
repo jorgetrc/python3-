8124bdc8586c2bec505b61736086de53cc15f1ec
jorgetrc <jorgetrc01@gmail.com>
2023-04-11 23:52:33 -0300

    Uso do print e string

---
+++ curso_udemy/aula3.py
@@ -0,0 +1,21 @@
+"""
+Docstring
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+
+# Aspas simples
+print('Texto exemplo')
+
+# Aspas duplas
+print("Texto exemplo")
+
+# Escape - Protege caracteres especiais para que sejam impressos com print.
+print("Texto \"exemplo\"")
+print('Texto "exemplo"')
+
+# r - Mostra todos os caracteres do print inclusive especiais.
+print(r"Texto \"exemplo\"")
