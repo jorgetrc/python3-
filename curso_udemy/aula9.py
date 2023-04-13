d6bb79369852fea53d0a4bdd9f2e1231a51ebda1
jorgetrc <jorgetrc01@gmail.com>
2023-04-12 23:49:35 -0300

    Operadores aritiméticos

---
+++ curso_udemy/aula9.py
@@ -0,0 +1,26 @@
+adicao = 10 + 10
+print('Adição', adicao)
+
+subtracao = 10 - 5
+print('Subtração', subtracao)
+
+multipicacao = 10 * 10
+print('Multiplicação', multipicacao)
+
+divisao = 10 / 2.2  # float
+print('Divisão', divisao)
+
+divisao_inteira = 10 // 2.2
+print('Divisão inteira', divisao_inteira)
+
+exponenciacao = 2 ** 10
+print('Exponenciação', exponenciacao)
+
+modulo = 55 % 2 # resto da divisão
+print('Módulo', modulo)
+
+print(10 % 8 == 0)  # 10 divisivel por 8? False
+print(16 % 8 == 0)  # 16 é Multiplo de 8 se for divisível por 8
+print(10 % 2 == 0)  # 10 é divisível por 2 então é par
+print(15 % 2 == 0)  # 15 é divisível por 2 então é impar
+print(16 % 2 == 0)  # 16 é divisível por 2 então é par
