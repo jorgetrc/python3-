6354461917fe5616c6d95379bd748fd2992275e4
jorgetrc <jorgetrc01@gmail.com>
2023-04-12 23:56:31 -0300

    Concatenação (+) e repetição (*) com operadores

---
+++ curso_udemy/aula10.py
@@ -0,0 +1,7 @@
+concatenacao = 'A' + 'B' + 'C' # Somente com strings
+print(concatenacao)
+
+a_dez_vezes = 'A' * 10  # String com inteiro
+tres_vezes_luiz = 3 * 'Luiz'
+print(a_dez_vezes)
+print(tres_vezes_luiz)
