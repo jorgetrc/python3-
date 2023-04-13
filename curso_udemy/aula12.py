f19692aa9bd1dd004ca97bb12db67e6343442a70
jorgetrc <jorgetrc01@gmail.com>
2023-04-13 00:17:19 -0300

    Exercicio IMC

---
+++ curso_udemy/aula12.py
@@ -0,0 +1,8 @@
+nome = 'Pedro'
+altura = 1.85
+peso = 105
+imc = peso / (altura * altura) #  IMC = peso / (altura x altura)
+
+print(nome, 'tem', altura, 'de altura,', 'pesa', peso, 'quilos e seu IMC é:', "\n", imc )
+
+... # Ellipisis
