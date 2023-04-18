6d488d1d59a89481b9e8e08d01aa31b97d753e7d
jorgetrc <jorgetrc01@gmail.com>
2023-04-18 11:42:10 -0300

    F-strings. limpando código

--- curso_udemy/aula13.py
+++ curso_udemy/aula13.py
@@ -1,11 +1,9 @@
-# Introdução a fstrings
+# f-strings
 
 nome = 'Pedro'
 altura = 1.85
 peso = 105
 imc = peso / (altura * altura) #  IMC = peso / (altura x altura)
-
-#print(nome, 'tem', altura, 'de altura,', 'pesa', peso, 'quilos e seu IMC é:', "\n", imc )
 
 resultado = f'{nome} tem {altura:.2f} de altura pesa {peso} quilos e seu IMC é: {imc:.2f}'
 print(resultado)
