7eb0eb578e6f392f70fc579d6462f27e0e0aa33c
jorgetrc <jorgetrc01@gmail.com>
2023-04-19 12:29:05 -0300

    Operadores de comparação (relacionais)

---
+++ curso_udemy/aula18.py
@@ -0,0 +1,17 @@
+"""
+Operadores de comparação (relacionais)
+OP      Significado     Exemplo (True)
+>       maior           2 > 1
+>=      mairo ou igual  2 >= 2
+<       menor           1 < 2
+<=      manor ou igual  2 <= 2
+==      igual           'a' == 'a'
+!=      diferente       'a' != 'b'
+"""
+maior = 2 > 1
+maior_ou_igual = 2 >= 2
+menor = 1 < 2
+menor_ou_igual = 2 <= 2
+igual = 'a' == 'a'
+diferente = 'a' != 'b'
+print(maior)
