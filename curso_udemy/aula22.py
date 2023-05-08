01a3d44c41eb82b0c461b0cba0ade508f79b15b6
jorgetrc <jorgetrc01@gmail.com>
2023-05-08 11:40:40 -0300

    Operadpr lógico not

---
+++ curso_udemy/aula22.py
@@ -0,0 +1,16 @@
+# Operador lógico "not"
+# Usado para inverter expressões
+# not True = False
+# not False = True
+
+print(not True) # False
+print(not False) # True
+
+# senha = input('Senha: ')
+
+# if senha != '123456':
+#     print('Senha incorreta.')
+
+# ----------------Ex: 2 --------------------
+# if not senha:
+#     print('Você não digitou nada')
