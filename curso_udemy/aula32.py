9e6558ae85fb64f68b9999f0331c4ad6701f605c
jorgetrc <jorgetrc01@gmail.com>
2023-05-14 17:49:23 -0300

    Documentação python e outras formas de trabalhar com strings

---
+++ curso_udemy/aula32.py
@@ -0,0 +1,8 @@
+"""
+https://docs.python.org/pt-br/3/library/stdtypes.html
+Imutáveis que vimos: str, int, float, bool
+"""
+string = 'pedro henrique'
+
+print(string.zfill(20))
+print(string.capitalize())
