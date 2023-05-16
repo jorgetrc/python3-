569dcd3133a08aedb1939e02f44a644a2c1efc93
jorgetrc <jorgetrc01@gmail.com>
2023-05-16 14:35:40 -0300

    While com else

---
+++ curso_udemy/aula38.py
@@ -0,0 +1,19 @@
+""" 
+while/else 
+Quando existe um break no while o else não é executado.
+
+"""
+string = 'Valorqualquer'
+
+i = 0
+while i < len(string):
+    letra = string[i]
+
+    if letra == ' ':
+        break
+
+    print(letra)
+    i += 1
+else:
+    print('Não encontrei um espaço na string.')
+print('Fora do while')
