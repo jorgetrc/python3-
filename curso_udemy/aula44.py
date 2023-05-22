6a0a1b03248052ebee750ee3010ce121af265b8a
jorgetrc <jorgetrc01@gmail.com>
2023-05-22 17:58:06 -0300

    Introdução às listas mutáveis em Python

---
+++ curso_udemy/aula44.py
@@ -0,0 +1,18 @@
+"""
+Listas em Python
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - íncices e fatiamento
+Métodos úteis: append, insert, pop, del, clear, extend, +
+"""
+
+#        +01234
+#        -54321
+sting = 'ABCDE'  # 5 caracteres (len)
+# print(bool([]))  # falsy
+# print(lista, type(lista))
+lista = [123, True, 'Pedro Henrique', 1.2, []]
+#print(lista[2].upper(), type(lista[2]))
+lista[2] = 'Maria'
+print(lista[2], type(lista[2]))
+print(lista)
